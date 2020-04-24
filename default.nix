e93883555d0679d8e3c9dc7da79da560ee61dd4b
Matthew Pickering <matthewtpickering@gmail.com>
2020-04-24 13:20:44 +0100

    Update nix files

---
+++ default.nix
@@ -0,0 +1,19 @@
+let
+  pin = import ((import ./nix/sources.nix).nixpkgs) {} ;
+in
+{ nixpkgs ? pin
+}:
+let
+  tl = nixpkgs.texlive.combine
+        { inherit (nixpkgs.texlive) scheme-full; };
+in
+nixpkgs.stdenv.mkDerivation {
+  name = "infinite-paper";
+  src = ./.;
+  buildInputs = [ tl
+                  nixpkgs.haskellPackages.lhs2tex
+                  nixpkgs.haskell.compiler.ghc881
+                  nixpkgs.xsv  ];
+  installPhase = ''cp build/paper.pdf $out'';
+
+}
